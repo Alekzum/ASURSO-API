27662bfc99b11c98eea01ef7b277564bff364f8b
Alekzum <68947902+Alekzum@users.noreply.github.com>
2025-03-05 16:37:51 +0400

    remove SID from tests/__init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,17 +9,18 @@ dotenv.load_dotenv(".env")
 
 
 pytestmark = pytest.mark.asyncio(loop_scope="session")
-async_client = httpx.AsyncClient(base_url="https://spo.asurso.ru")
-sync_client = httpx.Client(base_url="https://spo.asurso.ru")
+async_client = httpx.AsyncClient(base_url="https://spo.asurso.ru", timeout=30)
+sync_client = httpx.Client(base_url="https://spo.asurso.ru", timeout=30)
+env = os.environ
 
 async_asurso = AsyncASURSO(
-    login=os.environ["ASURSO_LOGIN"],
-    password=os.environ["ASURSO_PASSWORD"],
-    SID=os.environ["ASURSO_SID"],
+    login=env["ASURSO_LOGIN"],
+    password=env["ASURSO_PASSWORD"],
+    timeout=30,
 )
 
 sync_asurso = ASURSO(
-    login=os.environ["ASURSO_LOGIN"],
-    password=os.environ["ASURSO_PASSWORD"],
-    SID=os.environ["ASURSO_SID"],
+    login=env["ASURSO_LOGIN"],
+    password=env["ASURSO_PASSWORD"],
+    timeout=30,
 )
