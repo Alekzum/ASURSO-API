3b387e30a3f4557b0606bf31771c3844800017df
Alekzum <68947902+Alekzum@users.noreply.github.com>
2025-03-07 17:56:00 +0400

    camelCase -> snake_case via pydantic.Field

--- asurso_api/functions/info.py
+++ asurso_api/functions/info.py
@@ -1,68 +1,10 @@
 from ..utils import parse_response, MyAsyncClient, MyClient
 from typing import Protocol, List
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 import logging
 
 
 logger = logging.getLogger(__name__)
-
-
-class TitleItem(BaseModel):
-    language_code: str
-    value: str
-
-
-class LessonsMenu(BaseModel):
-    isEnabled: bool
-    sessionTableEnabled: bool
-    examinationEnabled: bool
-    profModuleExaminationEnabled: bool
-    courseworksEnabled: bool
-
-
-class EducationMenu(BaseModel):
-    isEnabled: bool
-    workingProgramsEnabled: bool
-
-
-class UsersMenu(BaseModel):
-    isEnabled: bool
-    enrolleesEnabled: bool
-    parentsEnabled: bool
-    expelledStudentsEnabled: bool
-    departmentsEnabled: bool
-
-
-class ForeignInstallation(BaseModel):
-    isEnabled: bool
-
-
-class AvailableLanguage(BaseModel):
-    key: str
-    value: str
-
-
-class Info(BaseModel):
-    title: List[TitleItem]
-    isFileStorageAvailable: bool
-    isSupplementaryEducationCertificatesAvailable: bool
-    isEditStudentFactualHoursAvailableForOrganization: bool
-    isFactualHoursAvailableSystemwide: bool
-    areChatsEnabled: bool
-    isErnEnabled: bool
-    isEmploymentEnabled: bool
-    isReportsMenuEnabled: bool
-    isPortfolioMenuEnabled: bool
-    isAdministrationMenuEnabled: bool
-    isOrgLicenseEnabled: bool
-    isOrgDetailsEnabled: bool
-    lessonsMenu: LessonsMenu
-    educationMenu: EducationMenu
-    usersMenu: UsersMenu
-    foreignInstallation: ForeignInstallation
-    defaultLanguage: str
-    availableLanguages: List[AvailableLanguage]
-    importEncoding: str
 
 
 class AsyncASURSO(Protocol):
@@ -77,6 +19,64 @@ class ASURSO(Protocol):
     _login: str
     _password: str
     _client: MyClient
+
+
+class TitleItem(BaseModel):
+    language_code: str = Field(..., alias="value")
+    value: str = Field(..., alias="value")
+
+
+class LessonsMenu(BaseModel):
+    is_enabled: bool = Field(..., alias="isEnabled")
+    session_table_enabled: bool = Field(..., alias="sessionTableEnabled")
+    examination_enabled: bool = Field(..., alias="examinationEnabled")
+    prof_module_examination_enabled: bool = Field(..., alias="profModuleExaminationEnabled")
+    courseworks_enabled: bool = Field(..., alias="courseworksEnabled")
+
+
+class EducationMenu(BaseModel):
+    is_enabled: bool = Field(..., alias="isEnabled")
+    working_programs_enabled: bool = Field(..., alias="workingProgramsEnabled")
+
+
+class UsersMenu(BaseModel):
+    is_enabled: bool = Field(..., alias="isEnabled")
+    enrollees_enabled: bool = Field(..., alias="enrolleesEnabled")
+    parents_enabled: bool = Field(..., alias="parentsEnabled")
+    expelled_students_enabled: bool = Field(..., alias="expelledStudentsEnabled")
+    departments_enabled: bool = Field(..., alias="departmentsEnabled")
+
+
+class ForeignInstallation(BaseModel):
+    is_enabled: bool = Field(..., alias="isEnabled")
+
+
+class AvailableLanguage(BaseModel):
+    key: str = Field(...)
+    value: str = Field(...)
+
+
+class Info(BaseModel):
+    title: List[TitleItem] = Field(..., alias="title")
+    is_file_storage_available: bool = Field(..., alias="isFileStorageAvailable")
+    is_supplementary_education_certificates_available: bool = Field(..., alias="isSupplementaryEducationCertificatesAvailable")
+    is_edit_student_factual_hours_available_for_organization: bool = Field(..., alias="isEditStudentFactualHoursAvailableForOrganization")
+    is_factual_hours_available_systemwide: bool = Field(..., alias="isFactualHoursAvailableSystemwide")
+    are_chats_enabled: bool = Field(..., alias="areChatsEnabled")
+    is_ern_enabled: bool = Field(..., alias="isErnEnabled")
+    is_employment_enabled: bool = Field(..., alias="isEmploymentEnabled")
+    is_reports_menu_enabled: bool = Field(..., alias="isReportsMenuEnabled")
+    is_portfolio_menu_enabled: bool = Field(..., alias="isPortfolioMenuEnabled")
+    is_administration_menu_enabled: bool = Field(..., alias="isAdministrationMenuEnabled")
+    is_org_license_enabled: bool = Field(..., alias="isOrgLicenseEnabled")
+    is_org_details_enabled: bool = Field(..., alias="isOrgDetailsEnabled")
+    lessons_menu: LessonsMenu = Field(..., alias="lessonsMenu")
+    education_menu: EducationMenu = Field(..., alias="educationMenu")
+    users_menu: UsersMenu = Field(..., alias="usersMenu")
+    foreign_installation: ForeignInstallation = Field(..., alias="foreignInstallation")
+    default_language: str = Field(..., alias="defaultLanguage")
+    available_languages: List[AvailableLanguage] = Field(..., alias="availableLanguages")
+    import_encoding: str = Field(..., alias="importEncoding")
 
 
 async def get_info_async(client: MyAsyncClient) -> Info:
